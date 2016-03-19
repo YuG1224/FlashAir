5885832f6e58afac9106dcea2b30ff367ff31f14
Yuji Yamaguchi <YuG1224@users.noreply.github.com>
2016-03-19 11:08:42 +0900

    post-image.luaを追加

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FlashAir-to-Slack
+
+- post-image.lua
+    - LUA_SD_EVENT=/lua/post-image.lua
+    - 最新の画像をSlackに投稿するスクリプト
---
+++ post-image.lua
@@ -0,0 +1,45 @@
+cjson = require "cjson"
+
+lastFileName = ""
+lastFilePath = ""
+maxMod = 0
+
+currentDir = "/DCIM"
+for dirName in lfs.dir(currentDir) do
+  dirPath = currentDir.."/"..dirName
+  subDir = lfs.attributes(dirPath, "mode")
+  if subDir == "directory" then
+    for fileName in lfs.dir(dirPath) do
+      filePath = dirPath.."/"..fileName
+      mod = lfs.attributes(filePath, "modification")
+      if mod > maxMod and string.match(fileName, "%JPG$") == "JPG" then
+        maxMod = mod
+        lastFileName = fileName
+        lastFilePath = filePath
+      end
+    end
+  end
+end
+
+boundary = "--61141483716826"
+contenttype = "multipart/form-data; boundary=" .. boundary
+token = ""
+channels = ""
+mes = "--" ..  boundary .. "\r\n"
+  .."Content-Disposition: form-data; name=\"file\"; filename=\""..lastFileName.."\"\r\n"
+  .."\r\n"
+  .."<!--WLANSDFILE-->\r\n"
+  .."--" .. boundary .. "--\r\n"
+
+
+blen = lfs.attributes(lastFilePath,"size") + string.len(mes) - 17
+b, c, h = fa.request{
+  url = "https://slack.com/api/files.upload?token="..token.."&channels="..channels,
+  method = "POST",
+  headers = {
+    ["Content-Length"] = tostring(blen),
+    ["Content-Type"] = contenttype
+  },
+  file = lastFilePath,
+  body = mes
+}
